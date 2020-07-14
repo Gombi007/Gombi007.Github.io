302521745f76c020ddf11dd97669852f0628797f
Gombi007 <gombos88@gmail.com>
2020-07-14 23:07:54 +0200

    This is a sandbox

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-function modify(){
-document.getElementById("simpleText").innerHTML = "";
+function modify() {
+    document.getElementById("simpleText").innerHTML = "";
 }
 
+function logins() {
+    let user = document.getElementById("user").value;
+    if (user == "admin") {
+        document.getElementById("wcm").innerHTML = "Welcome back" + "<br>" + user;
+    }
+}
+
+function galleryY() {
+    let gD = document.getElementById("galleryDIV");
+    let sD = document.getElementById("sorryDIV");
+        if (gD.style.display = "none") {
+        gD.style.display = "block";
+        sD.style.display = "none";
+    }
+}
+
+function galleryN() {
+    let gD = document.getElementById("galleryDIV");
+    let sD = document.getElementById("sorryDIV");
+        if (sD.style.display = "none") {
+        sD.style.display = "block";
+        gD.style.display = "none";
+    }
+}
